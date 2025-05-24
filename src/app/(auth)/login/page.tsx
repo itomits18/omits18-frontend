8bc266d98e3fbe7aac93fac00528f89d43a3daf2
AddienZakia <addienzakia1903@gmail.com>
2025-05-24 12:13:27 +0700

    feat: slicing login and register template background

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,9 @@
+import AuthBg from '../(container)/AuthBg';
+
+export default function page() {
+  return (
+    <section className="min-h-screen max-w-full bg-gradient-to-b from-white via-blue-100 via-20% to-green-200">
+      <AuthBg />
+    </section>
+  );
+}
