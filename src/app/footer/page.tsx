10735309be0e0b3ebe426893676258709c0790a0
laylizahra <laylinfz2804@gmail.com>
2025-06-17 11:53:20 +0700

    feat: add footer page

--- src/app/footer/page.tsx
+++ src/app/footer/page.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link';
 
 export default function Footer() {
   return (
-    <div className="bg-[#658578] relative w-full min-h-screen py-12 px-4 md:px-8 text-white overflow-hidden">
+    <div className="relative min-h-screen w-full overflow-hidden bg-[#658578] px-4 py-12 text-white md:px-8">
       <Image
         src="/images/footer/Group 188.png"
         alt="top"
@@ -27,11 +27,11 @@ export default function Footer() {
         alt="bottom"
         width="295"
         height="679"
-        className="absolute bottom-0 md:top-0 left-0 z-0 md:w-[325px]"
+        className="absolute bottom-0 left-0 z-0 md:top-0 md:w-[325px]"
       />
 
       {/* Judul */}
-      <div className="max-w-6xl mx-auto relative z-10">
+      <div className="relative z-10 mx-auto max-w-6xl">
         <div className="flex items-center gap-4 pl-8 md:pl-[30%]">
           <Image
             src="/images/footer/logo-omits.png"
@@ -40,16 +40,16 @@ export default function Footer() {
             height="100"
             className="object-contain md:w-[150px]"
           />
-          <div className="text-left mb-2">
+          <div className="mb-2 text-left">
             <Typography
               variant="h2"
-              className="text-2xl md:text-4xl text-[#E5DBA6] font-Cinzel font-bold mb-2"
+              className="mb-2 font-Cinzel text-2xl font-bold text-[#E5DBA6] md:text-4xl"
             >
               OMITS 18th
             </Typography>
             <Typography
               variant="h4"
-              className="text-sm md:text-xl text-[#E5DBA6] font-Lora font-semibold"
+              className="font-Lora text-sm font-semibold text-[#E5DBA6] md:text-xl"
             >
               Olimpiade Matematika ITS
             </Typography>
@@ -57,7 +57,7 @@ export default function Footer() {
         </div>
 
         <div className="hidden md:block">
-          <p className="mb-4 mx-auto max-w-3xl text-center text-xl font-Lora mt-2">
+          <p className="mx-auto mb-4 mt-2 max-w-3xl text-center font-Lora text-xl">
             Olimpiade Matematika ITS atau OMITS merupakan olimpiade matematika
             bergengsi tingkat nasional yang diselenggarakan untuk siswa jenjang
             SD, SMP, dan SMA sederajat serta mahasiswa.
@@ -65,9 +65,9 @@ export default function Footer() {
         </div>
 
         {/* OMITS */}
-        <div className="grid grid-cols-1 md:grid-cols-4 gap-8 mb-12 md:mt-10">
+        <div className="mb-12 grid grid-cols-1 gap-8 md:mt-10 md:grid-cols-4">
           <div className="md:hidden">
-            <p className="mb-4 text-center font-Lora mt-2">
+            <p className="mb-4 mt-2 text-center font-Lora">
               Olimpiade Matematika ITS atau OMITS merupakan olimpiade matematika
               bergengsi tingkat nasional yang diselenggarakan untuk siswa
               jenjang SD, SMP, dan SMA sederajat serta mahasiswa.
@@ -76,7 +76,7 @@ export default function Footer() {
 
           {/* Organized by */}
           <div>
-            <p className="text-xl md:text-2xl text-[#E5DBA6] font-bold mb-2 font-OZWizard">
+            <p className="mb-2 font-OZWizard text-xl font-bold text-[#E5DBA6] md:text-2xl">
               Organized by
             </p>
             <div className="flex gap-2">
@@ -102,7 +102,7 @@ export default function Footer() {
           <div>
             <Typography
               variant="h3"
-              className="text-xl md:text-2xl text-[#E5DBA6] font-bold mb-2 font-OZWizard"
+              className="mb-2 font-OZWizard text-xl font-bold text-[#E5DBA6] md:text-2xl"
             >
               Navigasi
             </Typography>
@@ -110,17 +110,17 @@ export default function Footer() {
               <div>
                 <ul className="space-y-2">
                   <li>
-                    <a href="#" className="hover:underline block">
+                    <a href="#" className="block hover:underline">
                       Tentang
                     </a>
                   </li>
                   <li>
-                    <a href="#" className="hover:underline block">
+                    <a href="#" className="block hover:underline">
                       Video Tutorial
                     </a>
                   </li>
                   <li>
-                    <a href="#" className="hover:underline block">
+                    <a href="#" className="block hover:underline">
                       Timeline
                     </a>
                   </li>
@@ -129,12 +129,12 @@ export default function Footer() {
               <div>
                 <ul className="space-y-2">
                   <li>
-                    <a href="#" className="hover:underline block">
+                    <a href="#" className="block hover:underline">
                       Testimonial
                     </a>
                   </li>
                   <li>
-                    <a href="#" className="hover:underline block">
+                    <a href="#" className="block hover:underline">
                       Contact Person
                     </a>
                   </li>
@@ -145,17 +145,17 @@ export default function Footer() {
 
           {/* Kompetisi */}
           <div>
-            <p className="text-xl md:text-2xl text-[#E5DBA6] font-bold mb-3 font-OZWizard">
+            <p className="mb-3 font-OZWizard text-xl font-bold text-[#E5DBA6] md:text-2xl">
               Kompetisi
             </p>
             <ul>
               <li>
-                <a href="#" className="hover:underline block">
+                <a href="#" className="block hover:underline">
                   OMITS
                 </a>
               </li>
               <li>
-                <a href="#" className="hover:underline block">
+                <a href="#" className="block hover:underline">
                   MISSION
                 </a>
               </li>
@@ -166,7 +166,7 @@ export default function Footer() {
           <div>
             <Typography
               variant="h3"
-              className="text-xl md:text-2xl text-[#E5DBA6] font-bold mb-4 font-OZWizard"
+              className="mb-4 font-OZWizard text-xl font-bold text-[#E5DBA6] md:text-2xl"
             >
               Contact Person
             </Typography>
@@ -184,10 +184,10 @@ export default function Footer() {
         </div>
 
         {/* Copyright Section + Social Media */}
-        <div className="border-t border-white/20 pt-6 mt-10 md:mt-20">
-          <div className="flex flex-col md:flex-row justify-between items-center gap-4">
+        <div className="mt-10 border-t border-white/20 pt-6 md:mt-20">
+          <div className="flex flex-col items-center justify-between gap-4 md:flex-row">
             {/* Copyright */}
-            <div className="text-xs text-center md:text-left">
+            <div className="text-center text-xs md:text-left">
               <div className="inline">Copyright © </div>
               <div className="inline text-[#E5DBA6]">
                 2025 OMITS HIMATIKA ITS
@@ -211,7 +211,7 @@ export default function Footer() {
                   alt="Instagram"
                   width={20}
                   height={20}
-                  className="hover:opacity-50 transition-opacity"
+                  className="transition-opacity hover:opacity-50"
                 />
               </Link>
 
@@ -225,7 +225,7 @@ export default function Footer() {
                   alt="TikTok"
                   width={20}
                   height={20}
-                  className="hover:opacity-50 transition-opacity"
+                  className="transition-opacity hover:opacity-50"
                 />
               </Link>
 
@@ -239,7 +239,7 @@ export default function Footer() {
                   alt="YouTube"
                   width={20}
                   height={20}
-                  className="hover:opacity-50 transition-opacity"
+                  className="transition-opacity hover:opacity-50"
                 />
               </Link>
 
@@ -253,7 +253,7 @@ export default function Footer() {
                   alt="Facebook"
                   width={24}
                   height={25}
-                  className="hover:opacity-50 transition-opacity"
+                  className="transition-opacity hover:opacity-50"
                 />
               </Link>
 
@@ -267,7 +267,7 @@ export default function Footer() {
                   alt="Facebook"
                   width={24}
                   height={25}
-                  className="hover:opacity-50 transition-opacity"
+                  className="transition-opacity hover:opacity-50"
                 />
               </Link>
             </div>
