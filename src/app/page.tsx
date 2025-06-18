9f90f05adcc24963424dbfc18917b87c3f51cc08
Layli Zahra <153082907+laylizahra@users.noreply.github.com>
2025-06-18 20:09:58 +0700

    feat: slicing navbar & footer (#33)
    * feat: add footer page
    
    * feat: add footer
    
    * feat: add footer page
    
    * feat: add footer
    
    * feat: add footer page
    
    * feat: slicing navbar and footer
    
    * feat: add footer page
    
    * feat: add footer
    
    ---------
    
    Co-authored-by: AddienZakia <addienzakia1903@gmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
-import { redirect } from 'next/navigation';
+import Layout from '@/layouts/Layout';
 
 export default function page() {
-  return redirect('/coming-soon');
+  return (
+    <Layout withFooter withNavbar>
+      <section className="min-h-screen bg-gradient-to-b from-[#A4CD9C] to-[#F2EAC5]">
+        hello
+      </section>
+    </Layout>
+  );
 }
