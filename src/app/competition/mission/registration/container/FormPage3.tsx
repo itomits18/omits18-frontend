7841f6d7b5cdbae6820f2886c6f7865b15ee3c90
AddienZakia <addienzakia1903@gmail.com>
2025-07-20 09:20:42 +0700

    fix: problem integration

--- src/app/competition/mission/registration/container/FormPage3.tsx
+++ src/app/competition/mission/registration/container/FormPage3.tsx
@@ -49,9 +49,9 @@ export default function FormPage3({
 
   return (
     <div className="flex w-full flex-col items-start gap-8 lg:flex-row">
-      <div className="relative order-2 w-full rounded-xl bg-white p-6 shadow-lg lg:order-1 lg:w-3/5">
+      <div className="relativeorder-2 w-full rounded-xl bg-white p-6 shadow-lg lg:order-1 lg:w-3/5">
         <div
-          className="absolute top-0 left-0 m-6 w-fit cursor-pointer rounded-full bg-blue-300 p-2 transition-all duration-200 hover:bg-blue-400 max-md:m-4"
+          className="absolute top-0 left-0 m-6 w-fit cursor-pointer rounded-full bg-blue-400 p-2 transition-all duration-200 hover:bg-blue-300"
           onClick={onBack}
         >
           <ChevronLeft size={20} className="text-neutral-main" />
