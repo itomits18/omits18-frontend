c3665e88c60086c59faff389d02294c215550cab
AddienZakia <addienzakia1903@gmail.com>
2025-09-11 18:48:49 +0700

    fix: font problem

--- src/lib/font.ts
+++ src/lib/font.ts
@@ -1,4 +1,3 @@
-import { Cinzel, Lora } from 'next/font/google';
 import localFont from 'next/font/local';
 
 export const OZWizard = localFont({
@@ -23,23 +22,110 @@ export const OZWizard = localFont({
   display: 'swap',
 });
 
-export const LoraFont = Lora({
-  subsets: ['latin'],
-  weight: ['400', '500', '600', '700'],
-  display: 'swap',
-  variable: '--font-Lora',
+// Cinzel (6 styles)
+export const CinzelFont = localFont({
+  src: [
+    {
+      path: '../app/fonts/Cinzel/Cinzel-Regular.ttf',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../app/fonts/Cinzel/Cinzel-Medium.ttf',
+      weight: '500',
+      style: 'normal',
+    },
+    {
+      path: '../app/fonts/Cinzel/Cinzel-SemiBold.ttf',
+      weight: '600',
+      style: 'normal',
+    },
+    {
+      path: '../app/fonts/Cinzel/Cinzel-Bold.ttf',
+      weight: '700',
+      style: 'normal',
+    },
+    {
+      path: '../app/fonts/Cinzel/Cinzel-ExtraBold.ttf',
+      weight: '800',
+      style: 'normal',
+    },
+    {
+      path: '../app/fonts/Cinzel/Cinzel-Black.ttf',
+      weight: '900',
+      style: 'normal',
+    },
+  ],
+  variable: '--font-cinzel',
 });
 
-export const CinzelFont = Cinzel({
-  subsets: ['latin'],
-  weight: ['400', '500', '600', '700', '800', '900'],
-  display: 'swap',
-  variable: '--font-Cinzel',
+// Lora (12 styles: normal + italic)
+export const LoraFont = localFont({
+  src: [
+    {
+      path: '../app/fonts/Lora/Lora-Regular.ttf',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../app/fonts/Lora/Lora-Italic.ttf',
+      weight: '400',
+      style: 'italic',
+    },
+
+    {
+      path: '../app/fonts/Lora/Lora-Medium.ttf',
+      weight: '500',
+      style: 'normal',
+    },
+    {
+      path: '../app/fonts/Lora/Lora-MediumItalic.ttf',
+      weight: '500',
+      style: 'italic',
+    },
+
+    {
+      path: '../app/fonts/Lora/Lora-SemiBold.ttf',
+      weight: '600',
+      style: 'normal',
+    },
+    {
+      path: '../app/fonts/Lora/Lora-SemiBoldItalic.ttf',
+      weight: '600',
+      style: 'italic',
+    },
+
+    {
+      path: '../app/fonts/Lora/Lora-Bold.ttf',
+      weight: '700',
+      style: 'normal',
+    },
+    {
+      path: '../app/fonts/Lora/Lora-BoldItalic.ttf',
+      weight: '700',
+      style: 'italic',
+    },
+  ],
+  variable: '--font-lora',
 });
 
-export const CinzelDecoFont = Cinzel({
-  subsets: ['latin'],
-  weight: ['400', '500', '600', '700', '800', '900'],
-  display: 'swap',
-  variable: '--font-CinzelDecorative',
-});
+// export const LoraFont = Lora({
+//   subsets: ['latin'],
+//   weight: ['400', '500', '600', '700'],
+//   display: 'swap',
+//   variable: '--font-Lora',
+// });
+
+// export const CinzelFont = Cinzel({
+//   subsets: ['latin'],
+//   weight: ['400', '500', '600', '700', '800', '900'],
+//   display: 'swap',
+//   variable: '--font-Cinzel',
+// });
+
+// export const CinzelDecoFont = Cinzel({
+//   subsets: ['latin'],
+//   weight: ['400', '500', '600', '700', '800', '900'],
+//   display: 'swap',
+//   variable: '--font-CinzelDecorative',
+// });
