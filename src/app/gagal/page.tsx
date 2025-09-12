ca9afc56a7a159149aeea9d95916c4ec943e4054
laylizahra <laylinfz2804@gmail.com>
2025-09-12 20:31:26 +0700

    feat: add pengumuman page

---
+++ src/app/gagal/page.tsx
@@ -0,0 +1,464 @@
+import Typography from '@/components/Typography';
+import { Button } from '@/components/ui/button';
+import Image from 'next/image';
+
+export default function HasilPengumuman() {
+  return (
+    <div
+      className="relative w-full min-h-screen overflow-hidden 
+        bg-gradient-to-t from-[#35547B] to-[#9C3022]"
+    >
+      {/* Mobile*/}
+      <Image
+        src="/images/semifinal/Asset 95 9.png"
+        width={288}
+        height={171}
+        alt="cloud1"
+        className="absolute top-[15%] md:hidden object-contain"
+      />
+
+      <Image
+        src="/images/semifinal/Asset 96 7.png"
+        width={283}
+        height={230}
+        alt="cloud1"
+        className="absolute top-[20%] right-0 md:hidden object-contain"
+      />
+
+      <Image
+        src="/images/semifinal/Asset 95 4.png"
+        width={251}
+        height={255}
+        alt="cloud2"
+        className="absolute top-[30%] md:hidden object-contain"
+      />
+
+      <Image
+        src="/images/semifinal/Asset 95 3.png"
+        width={168}
+        height={163}
+        alt="cloud3"
+        className="absolute top-[42%] right-0 md:hidden object-contain"
+      />
+
+      <Image
+        src="/images/semifinal/Asset 95 5.png"
+        width={273}
+        height={163}
+        alt="cloud4"
+        className="absolute top-[50%] md:hidden object-contain"
+      />
+
+      <Image
+        src="/images/semifinal/Asset 95 6.png"
+        width={167}
+        height={163}
+        alt="cloud5"
+        className="absolute top-[50%] right-0 md:hidden object-contain"
+      />
+
+      <Image
+        src="/images/semifinal/Asset 95 8.png"
+        width={262}
+        height={247}
+        alt="cloud6"
+        className="absolute top-[54%] opacity-30 right-0 md:hidden object-contain"
+      />
+
+      <Image
+        src="/images/semifinal/Asset 95 7.png"
+        width={267}
+        height={214}
+        alt="cloud7"
+        className="absolute top-[62%] opacity-30 md:hidden object-contain"
+      />
+
+      <Image
+        src="/images/semifinal/Asset 95 2.png"
+        width={260}
+        height={205}
+        alt="cloud8"
+        className="absolute top-[70%] right-0 opacity-30 md:hidden object-contain"
+      />
+
+      <Image
+        src="/images/semifinal/Asset 95 1.png"
+        width={364}
+        height={220}
+        alt="cloud9"
+        className="absolute top-[74%] md:hidden opacity-50 object-contain"
+      />
+
+      <Image
+        src="/images/semifinal/Asset 95 10.png"
+        width={311}
+        height={151}
+        alt="cloud10"
+        className="absolute bottom-0 right-0 opacity-50 md:hidden object-contain"
+      />
+
+      <Image
+        src="/images/semifinal/Asset 95 13.png"
+        width={172}
+        height={103}
+        alt="cloud11"
+        className="absolute top-[80%] md:hidden object-contain"
+      />
+
+      <Image
+        src="/images/semifinal/Asset 95 12.png"
+        width={176}
+        height={105}
+        alt="cloud12"
+        className="absolute top-[80%] right-0 md:hidden object-contain"
+      />
+
+      <Image
+        src="/images/semifinal/Asset 43 1.png"
+        width={43}
+        height={64}
+        alt="bottle"
+        className="absolute top-[80%] left-[10%] md:hidden object-contain"
+      />
+
+      <Image
+        src="/images/semifinal/Asset 42 1.png"
+        width={52}
+        height={55}
+        alt="bottle2"
+        className="absolute top-[80%] right-[10%] md:hidden object-contain"
+      />
+
+      <Image
+        src="/images/semifinal/Asset 46 1.png"
+        width={117}
+        height={63}
+        alt="hiasan"
+        className="absolute bottom-0 left-1/2 transform -translate-x-1/2 md:hidden object-contain"
+      />
+
+      <Image
+        src="/images/semifinal/logo.png"
+        width={205}
+        height={56}
+        alt="logo"
+        className="absolute top-[5%] left-1/2 transform -translate-x-1/2 object-contain"
+      />
+
+      <Image
+        src="/images/semifinal/Asset 30 1.png"
+        width={100}
+        height={56}
+        alt="map"
+        className="absolute top-[19%] left-1/2 transform -translate-x-1/2 z-10 object-contain"
+      />
+
+      {/* Desktop*/}
+      <Image
+        src="/images/semifinal/Group 235.png"
+        width={1389}
+        height={126}
+        alt="star"
+        className="absolute left-1/2 transform -translate-x-1/2 hidden md:block object-contain"
+      />
+
+      <Image
+        src="/images/semifinal/c1.png"
+        width={530}
+        height={386}
+        alt="cloud1"
+        className="absolute top-[15%] hidden md:block object-contain"
+      />
+
+      <Image
+        src="/images/semifinal/c 95 2.png"
+        width={385}
+        height={386}
+        alt="cloud2"
+        className="absolute top-[20%] right-0 hidden md:block object-contain"
+      />
+
+      <Image
+        src="/images/semifinal/c 95 4.png"
+        width={489}
+        height={386}
+        alt="cloud3"
+        className="absolute top-[34%] hidden md:block object-contain"
+      />
+
+      <Image
+        src="/images/semifinal/h 102 3.png"
+        width={155}
+        height={367}
+        alt="hiasan1"
+        className="absolute top-[35%] right-0 hidden md:block object-contain"
+      />
+
+      <Image
+        src="/images/semifinal/c 96 1.png"
+        width={329}
+        height={174}
+        alt="cloud4"
+        className="absolute top-[40%] right-0 hidden md:block opacity-30 object-contain"
+      />
+
+      <Image
+        src="/images/semifinal/h 102 2.png"
+        width={155}
+        height={367}
+        alt="hiasan1"
+        className="absolute top-[35%] hidden md:block object-contain"
+      />
+
+      <Image
+        src="/images/semifinal/c 97 1.png"
+        width={450}
+        height={199}
+        alt="cloud5"
+        className="absolute top-[50%] opacity-50 hidden md:block object-contain"
+      />
+
+      <Image
+        src="/images/semifinal/c 95 6.png"
+        width={559}
+        height={386}
+        alt="cloud5"
+        className="absolute top-[44%] left-[25%] opacity-30 hidden md:block object-contain"
+      />
+
+      <Image
+        src="/images/semifinal/c 95 9.png"
+        width={559}
+        height={386}
+        alt="cloud5"
+        className="absolute top-[44%] left-[50%] opacity-30 hidden md:block object-contain"
+      />
+
+      <Image
+        src="/images/semifinal/c 96 4.png"
+        width={570}
+        height={179}
+        alt="cloud5"
+        className="absolute opacity-50 top-[54%] left-1/2 transform -translate-x-1/2 hidden md:block object-contain"
+      />
+
+      <Image
+        src="/images/semifinal/c 95 5.png"
+        width={548}
+        height={241}
+        alt="cloud5"
+        className="absolute bottom-0 left-[20%] opacity-30 hidden md:block object-contain"
+      />
+
+      <Image
+        src="/images/semifinal/c 95 1.png"
+        width={630}
+        height={386}
+        alt="cloud5"
+        className="absolute bottom-0 left-[20%] hidden md:block object-contain"
+      />
+
+      <Image
+        src="/images/semifinal/c 96 2.png"
+        width={604}
+        height={172}
+        alt="cloud5"
+        className="absolute bottom-0 left-[50%] opacity-50 hidden md:block object-contain"
+      />
+
+      <Image
+        src="/images/semifinal/c 95 11.png"
+        width={206}
+        height={209}
+        alt="cloud5"
+        className="absolute bottom-[20%] right-0 opacity-50 hidden md:block object-contain"
+      />
+
+      <Image
+        src="/images/semifinal/c 96 3.png"
+        width={478}
+        height={179}
+        alt="cloud5"
+        className="absolute bottom-[10%] hidden opacity-30 md:block object-contain"
+      />
+
+      <Image
+        src="/images/semifinal/c 95 7.png"
+        width={408}
+        height={197}
+        alt="cloud5"
+        className="absolute bottom-[10%] opacity-30 left-[10%] hidden md:block object-contain"
+      />
+
+      <Image
+        src="/images/semifinal/c 97 1.png"
+        width={400}
+        height={199}
+        alt="cloud5"
+        className="absolute bottom-0 opacity-80 hidden md:block object-contain"
+      />
+
+      <Image
+        src="/images/semifinal/c 95 10.png"
+        width={508}
+        height={161}
+        alt="cloud5"
+        className="absolute bottom-0 left-[10%] opacity-50 hidden md:block object-contain"
+      />
+
+      <Image
+        src="/images/semifinal/c 97 2.png"
+        width={329}
+        height={174}
+        alt="cloud4"
+        className="absolute bottom-[10%] right-0 hidden md:block opacity-80 object-contain"
+      />
+
+      <Image
+        src="/images/semifinal/c 96 1.png"
+        width={429}
+        height={174}
+        alt="cloud4"
+        className="absolute bottom-0 right-0 opacity-50 hidden md:block object-contain"
+      />
+
+      <Image
+        src="/images/semifinal/h 46 1.png"
+        width={180}
+        height={95}
+        alt="cloud4"
+        className="absolute bottom-0 left-1/2 transform -translate-x-1/2 hidden md:block object-contain"
+      />
+
+      <Image
+        src="/images/semifinal/h 42 1.png"
+        width={88}
+        height={106}
+        alt="cloud4"
+        className="absolute bottom-0 right-0 hidden md:block object-contain"
+      />
+
+      <Image
+        src="/images/semifinal/h 42 2.png"
+        width={120}
+        height={130}
+        alt="cloud4"
+        className="absolute bottom-[10%] right-[10%] hidden md:block object-contain"
+      />
+
+      <Image
+        src="/images/semifinal/Group 236.png"
+        width={147}
+        height={143}
+        alt="cloud4"
+        className="absolute bottom-0 hidden md:block object-contain"
+      />
+
+      {/*Frame*/}
+      <div
+        className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 
+        bg-gradient-to-t from-[#FFFFFF] to-[#ce7367] px-4 md:px-12 py-6 rounded-2xl shadow-md w-[300px] h-[380px] md:w-[500px] md:h-[420px] 
+        font-Lora opacity-80 flex flex-col"
+      >
+        <Typography
+          font="Lora"
+          weight="bold"
+          variant="h1"
+          className="text-2xl md:text-4xl text-[#9C3022] text-center [text-shadow:_2px_3px_0_rgb(0_0_0_/_100%)] font-bold"
+        >
+          HASIL PENGUMUMAN
+        </Typography>
+
+        {/*Identitas*/}
+        <Typography
+          font="Lora"
+          weight="bold"
+          variant="h3"
+          className="text-sm md:text-lg text-[#5C2921] text-center px-2 mt-5"
+        >
+          Peserta dengan nomor identitas
+        </Typography>
+
+        <div className="w-full space-y-3 mb-3 mt-3">
+          <div className="flex justify-between items-start mb-3 mt-3">
+            <Typography
+              variant="p"
+              className="text-black-300 text-sm font-semibold w-1/3"
+            >
+              No Peserta:
+            </Typography>
+
+            <Typography
+              variant="p"
+              className="text-black-300 text-sm font-bold w-2/3 text-right"
+            >
+              017/17/01//001
+            </Typography>
+          </div>
+
+          <div className="flex justify-between items-start">
+            <Typography
+              variant="p"
+              className="text-black-300 text-sm font-semibold w-1/3"
+            >
+              Nama :
+            </Typography>
+
+            <Typography
+              variant="p"
+              className="text-black-300 text-sm font-bold w-2/3 text-right"
+            >
+              Grace Tiwy Sitanggang
+            </Typography>
+          </div>
+
+          <div className="flex justify-between items-start">
+            <Typography
+              variant="p"
+              className="text-black-300 text-sm font-semibold w-1/3"
+            >
+              Region :
+            </Typography>
+
+            <Typography
+              variant="p"
+              className="text-black-300 text-sm font-bold w-2/3 text-right"
+            >
+              Medan
+            </Typography>
+          </div>
+        </div>
+
+        <Typography
+          variant="h5"
+          className="text-xs text-center md:font-bold md:text-lg mb-3"
+        >
+          Dinyatakan{' '}
+          <span className="bg-[#9C3022] text-white px-2 py-1 rounded-xl mx-1">
+            Tidak Lolos
+          </span>{' '}
+          ke tahap Semi Final
+        </Typography>
+
+        <Typography
+          font="Lora"
+          weight="bold"
+          variant="h3"
+          className="text-sm md:text-lg text-[#5C2921] text-center mt-1"
+        >
+          Silahkan bergabung ke Grup WhatsApp untuk informasi selanjutnya
+        </Typography>
+
+        <div className="text-center mt-4 flex flex-row gap-2 md:px-10 justify-between">
+          <Button className="bg-[#9C3022] hover:bg-[#ff7d59] text-white text-xs py-2 px-3">
+            Kembali ke Beranda
+          </Button>
+
+          <Button className=" bg-white hover:bg-gray-100 text-gray-700 border border-gray-300 font-semibold py-2 px-2">
+            SK Pengumuman
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+}
